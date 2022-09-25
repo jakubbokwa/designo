b596d8eb660e7cc7ef80ae7188810a69e6118674
Jakub Bokwa <jakubbokwa@wp.pl>
2022-09-25 17:13:16 +0200

    fix: cta margin

--- src/components/cta/CallToAction.tsx
+++ src/components/cta/CallToAction.tsx
@@ -4,10 +4,10 @@ import { ReactComponent as BgPatternTwoCircles } from "../../assets/shared/deskt
 
 const CallToAction = () => {
   return (
-    <div className="overflow-hidden relative bg-primary-peach rounded-2xl mx-6 text-white text-center top-[189px]">
+    <div className="overflow-hidden relative bg-primary-peach rounded-2xl mx-6 md:mx-10 text-white text-center top-[189px]">
       <BgPatternThreeCircles className="absolute -scale-x-100 -top-[102px] -right-[14px] md:-right-[0px] md:-left-[55px]" />
       <BgPatternTwoCircles className="hidden md:block absolute -scale-x-100 rotate-90 md:top-[44px] md:left-[382px]" />
-      <div className="relative mx-6 md:mx-10 py-16 md:py-14 flex flex-col items-center z-20">
+      <div className="relative mx-6 py-16 md:py-14 flex flex-col items-center z-20">
         <h2 className="text-4xl-mobile md:text-4xl font-medium mx-6 md:w-1/2">
           Let's talk about your project
         </h2>
